2f485f52066c87d66e3f8bae7048a5348c2c754f
Redderpears <zwlukaszek@gmail.com>
2025-12-22 22:42:24 -0500

    added geodesy

--- include/yet_another_gps_publisher/yet_another_gps_publisher_node.hpp
+++ include/yet_another_gps_publisher/yet_another_gps_publisher_node.hpp
@@ -2,6 +2,7 @@
 
 #include "rclcpp/rclcpp.hpp"
 #include "std_msgs/msg/string.hpp"
+#include "geodesy/utm.h"
 
 // yet_another_gps_publisher_node.cpp
 class yet_another_gps_publisher : public rclcpp::Node {
@@ -9,6 +10,7 @@ private:
     rclcpp::Publisher<std_msgs::msg::String>::SharedPtr pub;
 
     rclcpp::Subscription<std_msgs::msg::String>::SharedPtr sub;
+    geodesy::UTMPose currentPose;
 
 public:
     yet_another_gps_publisher(const rclcpp::NodeOptions& options);
